921b26fd44ae0b6f3ad96dfb2d1bb41ac836f1fa
Timothée Flutre <timothee.flutre@inra.fr>
2019-04-03 16:04:43 +0200

    handle cases with single data point in BBI() and errors in genoAr1Coef()

--- R/apple.R
+++ R/apple.R
@@ -42,14 +42,18 @@ BBI <- function(dat, coln.prod="prod", coln.geno="geno",
   ## compute the index
   for(g in 1:G){
     geno <- levels(dat[[coln.geno]])[g]
-    trees.g <- unique(dat[dat[[coln.geno]] == geno, coln.rep])
+    trees.g <- as.character(unique(dat[dat[[coln.geno]] == geno, coln.rep]))
     R.g <- length(trees.g)
+    if(verbose > 0){
+      txt <- paste0(geno, ": ", R.g)
+      write(txt, stdout())
+    }
     if(R.g == 0)
       next
 
-    num <- 0
-    denom <- 0
     if(type == "classic"){
+      num <- 0
+      denom <- 0
       for(r in 1:R.g){
         tree <- trees.g[r]
         y.g.r <- dat[dat[[coln.geno]] == geno &
@@ -57,13 +61,16 @@ BBI <- function(dat, coln.prod="prod", coln.geno="geno",
                      coln.prod]
         T.gr <- length(y.g.r)
         denom <- denom + T.gr - 1
-        for(t in 2:T.gr)
-          if(y.g.r[t-1] != 0 || y.g.r[t] != 0)
-            num <- num +
-              abs(y.g.r[t] - y.g.r[t-1]) /
-              (y.g.r[t-1] + y.g.r[t])
+        if(T.gr > 1){
+          for(t in 2:T.gr)
+            if(y.g.r[t-1] != 0 || y.g.r[t] != 0)
+              num <- num +
+                abs(y.g.r[t] - y.g.r[t-1]) /
+                (y.g.r[t-1] + y.g.r[t])
+        }
       }
-      out[g] <- (2 * num) / denom
+      if(denom != 0)
+        out[g] <- (2 * num) / denom
 
     } else if(type %in% c("norm", "res_norm")){
       num.num <- 0
@@ -84,18 +91,21 @@ BBI <- function(dat, coln.prod="prod", coln.geno="geno",
         num.denom <- num.denom + T.gr - 1
         denom.num <- denom.num + y.g.r[1]
         denom.denom <- denom.denom + T.gr
-        for(t in 2:T.gr){
-          denom.num <- denom.num + y.g.r[t]
-          if(type == "norm"){
-            num.num <- num.num +
-              abs(y.g.r[t] - y.g.r[t-1])
-          } else if(type == "res_norm")
-            num.num <- num.num +
-              abs(epsilon.g.r[t] - epsilon.g.r[t-1])
+        if(T.gr > 1){
+          for(t in 2:T.gr){
+            denom.num <- denom.num + y.g.r[t]
+            if(type == "norm"){
+              num.num <- num.num +
+                abs(y.g.r[t] - y.g.r[t-1])
+            } else if(type == "res_norm")
+              num.num <- num.num +
+                abs(epsilon.g.r[t] - epsilon.g.r[t-1])
+          }
         }
       }
-      out[g] <- (num.num / num.denom) /
-        (denom.num / denom.denom)
+      if(all(denom.num != 0, denom.denom != 0))
+        out[g] <- (num.num / num.denom) /
+          (denom.num / denom.denom)
     }
 
   }
@@ -111,39 +121,63 @@ BBI <- function(dat, coln.prod="prod", coln.geno="geno",
 ##' @param coln.geno column name of the genotype
 ##' @param coln.rep column name of the tree replicate
 ##' @param coln.year column name of the year (the column data should be convertible to \code{numeric})
+##' @param method fitting method passed to \code{arima} (ML/CSS/ML|CSS); if \code{"ML|CSS"}, first "ML" then "CSS" will be tried
+##' @param transform.pars if true, the AR parameters are transformed to ensure that they remain in the region of stationarity
+##' @param optim.control list of control parameters for \code{optim}
+##' @param verbose verbosity level (0/1)
 ##' @return vector
 ##' @author Timothee Flutre
 ##' @export
 genoAr1Coef <- function(dat, coln.epsilon="residual", coln.geno="geno",
-                        coln.rep="tree", coln.year="year"){
+                        coln.rep="tree", coln.year="year",
+                        method="ML|CSS", transform.pars=TRUE,
+                        optim.control=list(),
+                        verbose=0){
   stopifnot(is.data.frame(dat),
             coln.epsilon %in% colnames(dat),
             coln.rep %in% colnames(dat),
             coln.geno %in% colnames(dat),
-            coln.year %in% colnames(dat))
+            coln.year %in% colnames(dat),
+            method %in% c("ML", "CSS", "ML|CSS", "CSS|ML"))
 
+  dat <- droplevels(dat)
   dat[[coln.geno]] <- as.factor(dat[[coln.geno]])
   genos <- levels(dat[[coln.geno]])
   G <- nlevels(dat[[coln.geno]])
 
   fit.ar1.gr <-
-    do.call(rbind, lapply(1:G, function(g){
+    lapply(1:G, function(g){
       geno <- genos[g]
       trees.g <- as.character(unique(dat[dat[[coln.geno]] == geno, coln.rep]))
       R.g <- length(trees.g)
-      out <- c()
+      out <- rep(NA, R.g)
       for(r in 1:R.g){
+        if(verbose > 0){
+          txt <- paste0(geno, ": ", r, "/", R.g)
+          write(txt, stdout())
+        }
         tmp <- droplevels(dat[dat[[coln.geno]] == geno &
                               dat[[coln.rep]] == trees.g[r],])
-        fit <- stats::arima(x=tmp[[coln.epsilon]], order=c(1,0,0),
-                            method="ML") ## CSS can return an error
-        out <- c(out, stats::coef(fit)["ar1"])
+        if(nrow(tmp) > 1){
+          for(m in strsplit(method, "\\|")[[1]]){
+            fit <- try(stats::arima(x=tmp[[coln.epsilon]],
+                                    order=c(1,0,0),
+                                    method=m,
+                                    transform.pars=transform.pars,
+                                    optim.control=optim.control),
+                       silent=ifelse(verbose <= 0, TRUE, FALSE))
+            if(class(fit) != "try-error")
+              break
+          }
+          if(class(fit) == "Arima")
+            out[r] <- stats::coef(fit)["ar1"]
+        }
       }
       out
-    }))
-  rownames(fit.ar1.gr) <- genos
+    })
+  names(fit.ar1.gr) <- genos
 
-  fit.ar1.g <- apply(fit.ar1.gr, 1, mean)
+  fit.ar1.g <- sapply(fit.ar1.gr, mean)
 
   return(fit.ar1.g)
 }
